93823647fe08e37986d6aca2320a2c66094f3f91
rohan future devoloper <xrohaislam373@gmail.com>
2023-08-11 15:48:36 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <HelmetProvider>
         <div className='max-w-screen-xl mx-auto'>
           <RouterProvider router={router} />
-        </div>
+        </div>  
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
